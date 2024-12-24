e13f390da500701bbcb02a8f16bdde8f7dc75eb2
unknown <zacksevendeef@gmail.com>
2024-12-24 15:24:04 +0700

    menambahkan Test soal beserta img

--- my-vue-app/src/components/nav/FormComponent.jsx
+++ my-vue-app/src/components/nav/FormComponent.jsx
@@ -19,20 +19,29 @@ function FromComponent () {
         <div>
             <h2>kolom Curhat</h2>
             <form onSubmit={handleSubmint} className="form">
-                <div>
-                    <label htmlFor="nama" className="form label">Nama :</label>
-                    <input className="form input" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+                <div className="form--content">
+                    <div>
+                        <div>
+                            <label htmlFor="nama" className="form label">Nama :</label>
+                            <input className="form input" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+                        </div>
+                        <div>
+                            <label htmlFor="email" className="form label">Email :</label>
+                            <input className="form input" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                        </div>
+                        <div>
+                            <label htmlFor="message" className="form label">Curhatan :</label>
+                            <textarea  name="message" id="message" value={message} onChange={(e) => setMessage(e.target.value)} required>
+                                isi dengan apa yang ada di pikiran anda</textarea>
+                        </div>
+                    </div>
+                   
                 </div>
-                <div>
-                    <label htmlFor="email" className="form label">Email :</label>
-                    <input className="form input" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-                </div>
-                <div>
-                    <label htmlFor="message" className="form label">Curhatan :</label>
-                    <textarea  name="message" id="message" value={message} onChange={(e) => setMessage(e.target.value)} required>
-                        isi dengan apa yang ada di pikiran anda</textarea>
-                </div>
-                    <button className="form button" type="submit">Submit</button>
+
+                <button className="form button" type="submit">Submit</button>
+                <img className="form--img" src="../../../img/pngwing.png" alt="gambar" />
+                    
+                
             </form>
         </div>
     )
